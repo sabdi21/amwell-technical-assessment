3391052260fce34d2b966ed66c93bf2a55ff1707
sabdi21 <sabdi21@hotmail.com>
2020-05-06 22:13:07 -0700

    dom set-up works

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const url = "https://www.iwillfearnoevil.com/screen/string.txt";
 let dataArray;
 
+//DOM references
+let button = document.getElementById('button')
+let allData = document.getElementById('all-data');
+
 
 //fetch data from URL
 function fetchData(url) {
@@ -21,3 +25,21 @@ function fetchData(url) {
 //Sending the request through a proxy
 fetchData(proxyUrl + url)
 
+// helper function to clean and sort the data
+function cleanData(data) {
+    let result = []
+    let string = data.split((/\r?\n/))
+    for (let i = 0; i < string.length; i++) {
+        result.push(parseInt(string[i]))  
+    }
+      // allData.textContent = dataArray
+    let sorted = result.sort((a, b) => a - b)
+    const cleanArray = new Set(sorted)
+    return finalArray = [...cleanArray]
+}
+
+// event listener to retrieve data
+button.addEventListener('click',() => {
+    allData.textContent = dataArray
+    console.log('testing', cleanData(dataArray))
+})
